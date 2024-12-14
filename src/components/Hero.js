95e75b4ef825a84921a92282651e1113893a8854
Argha Mallick <arghamallick5@gmail.com>
2024-12-14 18:43:10 +0530

    responsive hero and details

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,11 +10,11 @@ const Hero = () => {
                 <Image src='/assets/hero/techstorm.svg' alt='techstorm' width={1217} height={187} className='w-full' />
 
                 <div className="flex w-full items-center">
-                    <Image src='/assets/hero/25.svg' alt='25' width={334} height={176} className='w-3/12' />
+                    <Image src='/assets/hero/25.svg' alt='25' width={334} height={176} className='w-5/12 md:w-3/12' />
                 </div>
 
                 <div className="flex items-center">
-                    <p className={`${righteous.className} w-9/12 text-balance text-xl md:text-2xl text-[#BEBEBE]`}>Discover the essence of innovation at <span className='bg-[#AF52DE] text-black rounded-2xl py-1 px-2 capitalize font-ribes'>TECHSTORM 2.25</span>, the pinnacle of te chnological brilliance. As the flagship technical fest of B. P. Poddar Institute of Management and Technology, TECHSTORM has been a beacon of inspiration for the technical community for over a decade. In 2024, brace yourself for the grand celebration of its 11th edition, promising an experience that transcends boundaries and ignites the spirit of innovation on an unprecedented scale.</p>
+                    <p className={`${righteous.className} w-11/12 md:w-9/12 text-balance text-xl md:text-2xl text-[#BEBEBE] leading-tight md:leading-normal`}>Discover the essence of innovation at <span className='bg-[#AF52DE] text-black rounded-2xl md:py-1 px-2 capitalize font-ribes'>TECHSTORM 2.25</span>, the pinnacle of te chnological brilliance. As the flagship technical fest of B. P. Poddar Institute of Management and Technology, TECHSTORM has been a beacon of inspiration for the technical community for over a decade. In 2024, brace yourself for the grand celebration of its 11th edition, promising an experience that transcends boundaries and ignites the spirit of innovation on an unprecedented scale.</p>
                 </div>
             </div>
 
