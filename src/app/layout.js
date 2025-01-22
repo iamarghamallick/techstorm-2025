2ea18208a9af91961539f3df4439c2ed9651401d
Argha Mallick <arghamallick5@gmail.com>
2025-01-22 22:24:05 +0530

    nextjs-toploading bar added

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import StarsBackground from "@/components/StarsBackground";
+import NextTopLoader from 'nextjs-toploader';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,6 +25,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="relative overflow-x-hidden">
+        <NextTopLoader
+          color="#2299DD"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={true}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+          template='<div class="bar" role="bar"><div class="peg"></div></div><div class="spinner" role="spinner"><div class="spinner-icon"></div></div>' // spinner-icon class is kept hidden on globals.css
+          zIndex={1600}
+          showAtBottom={false}
+        />
 
         <StarsBackground />
 
