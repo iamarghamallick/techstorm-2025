abebc23e29b4889fc11855835b88fb3514976722
Argha Mallick <arghamallick5@gmail.com>
2025-01-30 20:10:40 +0530

    hidden schedule page with prev year data

--- src/app/schedule/page.js
+++ src/app/schedule/page.js
@@ -1,11 +1,75 @@
-import LetterGlitch from "@/components/ReactBits/LetterGlitch";
-import { poppins } from "../fonts";
+import { poppins, righteous } from "../fonts";
+import { day1, day2, day3 } from "@/lib/schedule";
 
 export default function Schedule() {
     return (
         <main className="mt-16 md:mt-24 schedule-container min-h-screen flex justify-center p-2 md:p-8 py-8">
-            <div className="container">
-                <h1 className={`${poppins.className} text-4xl text-center text-white font-bold`}>Schedule - Coming Soon</h1>
+            <div className="container px-2 md:px-20">
+                <h1 className={`${poppins.className} mb-8 md:mb-0 text-center text-2xl font-bold`}>Schedule</h1>
+                <h1 className={`${poppins.className} text-center text-xl font-semibold text-gray-300 my-48`}>Coming Soon</h1>
+
+                {/* Day 1 : 20th March 2025 */}
+                {/* <div className="day">
+                    <h2 className={`${poppins.className} text-2xl text-white font-bold my-4`}>20th March</h2>
+                    <div className="schedule-heading">
+                        <div className="schedule-item grid grid-cols-3 gap-4 p-2 md:p-4 bg-gray-700 rounded-t-lg">
+                            <div className={`${righteous.className} m-auto timing font-bold text-lg md:text-xl`}>Timing</div>
+                            <div className={`${righteous.className} m-auto name font-bold text-lg md:text-xl`}>Event</div>
+                            <div className={`${righteous.className} m-auto round font-bold text-lg md:text-xl`}>Round</div>
+                        </div>
+                    </div>
+                    <div className="schedule">
+                        {day1.map((event, index) => (
+                            <div key={index} className="schedule-item grid grid-cols-3 gap-1 md:gap-4 py-4 border-t border-gray-300">
+                                <div className={`${poppins.className} m-auto text-center text-sm md:text-base timing`}>{event.timing}</div>
+                                <div className={`${poppins.className} m-auto text-center text-sm md:text-base name`}>{event.name}</div>
+                                <div className={`${poppins.className} m-auto text-center text-sm md:text-base round`}>{event.round}</div>
+                            </div>
+                        ))}
+                    </div>
+                </div> */}
+
+                {/* Day 2 : 21st March 2025 */}
+                {/* <div className="day">
+                    <h2 className={`${poppins.className} text-2xl text-white font-bold my-4`}>21st March</h2>
+                    <div className="schedule-heading">
+                        <div className="schedule-item grid grid-cols-3 gap-4 p-2 md:p-4 bg-gray-700 rounded-t-lg">
+                            <div className={`${righteous.className} m-auto timing font-bold text-lg md:text-xl`}>Timing</div>
+                            <div className={`${righteous.className} m-auto name font-bold text-lg md:text-xl`}>Event</div>
+                            <div className={`${righteous.className} m-auto round font-bold text-lg md:text-xl`}>Round</div>
+                        </div>
+                    </div>
+                    <div className="schedule">
+                        {day2.map((event, index) => (
+                            <div key={index} className="schedule-item grid grid-cols-3 gap-1 md:gap-4 py-4 border-t border-gray-300">
+                                <div className={`${poppins.className} m-auto text-center text-sm md:text-base timing`}>{event.timing}</div>
+                                <div className={`${poppins.className} m-auto text-center text-sm md:text-base name`}>{event.name}</div>
+                                <div className={`${poppins.className} m-auto text-center text-sm md:text-base round`}>{event.round}</div>
+                            </div>
+                        ))}
+                    </div>
+                </div> */}
+
+                {/* Day 3 : 22nd March 2025 */}
+                {/* <div className="day">
+                    <h2 className={`${poppins.className} text-2xl text-white font-bold my-4`}>22nd March</h2>
+                    <div className="schedule-heading">
+                        <div className="schedule-item grid grid-cols-3 gap-4 p-2 md:p-4 bg-gray-700 rounded-t-lg">
+                            <div className={`${righteous.className} m-auto timing font-bold text-lg md:text-xl`}>Timing</div>
+                            <div className={`${righteous.className} m-auto name font-bold text-lg md:text-xl`}>Event</div>
+                            <div className={`${righteous.className} m-auto round font-bold text-lg md:text-xl`}>Round</div>
+                        </div>
+                    </div>
+                    <div className="schedule">
+                        {day3.map((event, index) => (
+                            <div key={index} className="schedule-item grid grid-cols-3 gap-1 md:gap-4 py-4 border-t border-gray-300">
+                                <div className={`${poppins.className} m-auto text-center text-sm md:text-base timing`}>{event.timing}</div>
+                                <div className={`${poppins.className} m-auto text-center text-sm md:text-base name`}>{event.name}</div>
+                                <div className={`${poppins.className} m-auto text-center text-sm md:text-base round`}>{event.round}</div>
+                            </div>
+                        ))}
+                    </div>
+                </div> */}
             </div>
         </main>
     )
---
+++ src/lib/schedule.js
@@ -0,0 +1,318 @@
+export const day1 = [
+    {
+        sl: 1,
+        timing: "9:30 A.M. - 10:30 A.M.",
+        name: "Inauguration (B block Seminar Hall)",
+        round: ""
+    },
+    {
+        sl: 2,
+        timing: "10:30 A.M. - 12:00 P.M.",
+        name: "Omegatrix (C Block Auditorium 7th Floor)",
+        round: "Prelims"
+    },
+    {
+        sl: 3,
+        timing: "10:30 A.M. - 1:30 P.M.",
+        name: "FIFA (C Block Lab 8)",
+        round: "Prelims"
+    },
+    {
+        sl: 4,
+        timing: "10:30 A.M. - 1:30 P.M.",
+        name: "Passion with reels (B block Seminar Hall)",
+        round: "Final"
+    },
+    {
+        sl: 5,
+        timing: "12:15 P.M. - 1:30 P.M.",
+        name: "PES (C Block Auditorium 7th Floor)",
+        round: "Prelims"
+    },
+    {
+        sl: 6,
+        timing: "12:00 P.M. - 1:30 P.M.",
+        name: "Code- Bee ( C block Lab 101, 102, 104, 105, 601,602)",
+        round: "Qualifier Round 1"
+    },
+    {
+        sl: 7,
+        timing: "10:30 A.M. - 1:30 P.M.",
+        name: "Ro-Terrance (B-Block 204)",
+        round: "Qualifier Round"
+    },
+    {
+        sl: 7,
+        timing: "10:30 A.M. - 1:30 P.M.",
+        name: "Ro-Navigator (B Block 205)",
+        round: "Qualifier Round"
+    },
+    {
+        sl: 7,
+        timing: "10:30 A.M. - 1:30 P.M.",
+        name: "Ro-Soccer (B-Block 202)",
+        round: "Qualifier Round"
+    },
+    {
+        sl: 7,
+        timing: "10:30 A.M. - 1:30 P.M.",
+        name: "Aqua Race (C Block Boy’s",
+        round: "Qualifier Round"
+    },
+    {
+        sl: 7,
+        timing: "10:30 A.M. - 1:30 P.M.",
+        name: "Common Room)",
+        round: "Qualifier Round"
+    },
+    {
+        sl: 7,
+        timing: "10:30 A.M. - 1:30 P.M.",
+        name: "Khet (B Block 404)",
+        round: "Qualifier Round"
+    },
+    {
+        sl: 8,
+        timing: "10:30 A.M. - 1:30 P.M.",
+        name: "Ro-Combat (A Block)",
+        round: "Bot Testing"
+    },
+    {
+        sl: 9,
+        timing: "1:30 P.M. - 2:30 P.M.",
+        name: "LUNCH BREAK"
+    }, {
+        sl: 10,
+        timing: "2:30 P.M. - 5:30 P.M.",
+        name: "PES (C Block Auditorium 7th Floor)",
+        round: "Prelims"
+    },
+    {
+        sl: 11,
+        timing: "2:30 P.M. - 5:30 P.M.",
+        name: "Code-Bee (C Block 101,102, 104,105, 601,602)",
+        round: "Qualifier Round 2"
+    },
+    {
+        sl: 12,
+        timing: "2:30 P.M. - 5:30 P.M.",
+        name: "Ro-Terrance (B-Block 204)",
+        round: "Qualifier Round"
+    },
+    {
+        sl: 12,
+        timing: "2:30 P.M. - 5:30 P.M.",
+        name: "Ro-Navigator (B Block 205)",
+        round: "Qualifier Round"
+    },
+    {
+        sl: 12,
+        timing: "2:30 P.M. - 5:30 P.M.",
+        name: "Ro-Soccer (B-Block 202)",
+        round: "Qualifier Round"
+    },
+    {
+        sl: 12,
+        timing: "2:30 P.M. - 5:30 P.M.",
+        name: "Aqua Race (C Block Boy’s",
+        round: "Qualifier Round"
+    },
+    {
+        sl: 12,
+        timing: "2:30 P.M. - 5:30 P.M.",
+        name: "Common Room)",
+        round: "Qualifier Round"
+    },
+    {
+        sl: 12,
+        timing: "2:30 P.M. - 5:30 P.M.",
+        name: "Khet (B Block 404)",
+        round: "Qualifier Round"
+    },
+    {
+        sl: 13,
+        timing: "2:30 P.M. - 5:30 P.M.",
+        name: "FIFA (C Block Lab 8)",
+        round: "Prelims"
+    },
+    {
+        sl: 14,
+        timing: "02:30PM - 5:30 PM",
+        name: "Appmania (B block Seminar Hall)",
+        round: "Final"
+    }
+]
+
+export const day2 = [
+    {
+        sl: 1,
+        timing: "10:00 A.M. -11:30 A.M.",
+        name: "Omegatrix (C Block Auditorium 7th Floor)",
+        round: "Final"
+    },
+    {
+        sl: 2,
+        timing: "10:00 A.M. -1:00 P.M.",
+        name: "Creative Canvas ( C block Lab 403)",
+        round: "Final"
+    },
+    {
+        sl: 3,
+        timing: "11:00 A.M. - 12:30 P.M.",
+        name: "Code Bee ( C block Lab 101, 102, 104, 105, 601,602)",
+        round: "Final"
+    },
+    {
+        sl: 4,
+        timing: "10:30 A.M. - 1:30 P.M.",
+        name: "Ro-Terrance (B-Block 204)",
+        round: "Final"
+    },
+    {
+        sl: 4,
+        timing: "10:30 A.M. - 1:30 P.M.",
+        name: "Ro-Navigator (B Block 205)",
+        round: "Final"
+    },
+    {
+        sl: 4,
+        timing: "10:30 A.M. - 1:30 P.M.",
+        name: "Ro-Soccer (B-Block 202)",
+        round: "Final"
+    },
+    {
+        sl: 4,
+        timing: "10:30 A.M. - 1:30 P.M.",
+        name: "Aqua Race (C Block Boy’s",
+        round: "Final"
+    },
+    {
+        sl: 4,
+        timing: "10:30 A.M. - 1:30 P.M.",
+        name: "Common Room)",
+        round: "Final"
+    },
+    {
+        sl: 4,
+        timing: "10:30 A.M. - 1:30 P.M.",
+        name: "Khet (B Block 404)",
+        round: "Final"
+    },
+    {
+        sl: 5,
+        timing: "10:30 A.M. - 1:30 P.M.",
+        name: "FIFA (C Block Lab 8)",
+        round: "Final"
+    },
+    {
+        sl: 6,
+        timing: "12:00 P.M. - 1:30 P.M.",
+        name: "PES (C Block Auditorium 7th Floor)",
+        round: "Final"
+    },
+    {
+        sl: 7,
+        timing: "10:30 A.M. - 1:30 P.M.",
+        name: "Tech Hunt (C block Student Activity Room)",
+        round: "Prelims"
+    },
+    {
+        sl: 8,
+        timing: "1:30 P.M. - 2:30 P.M.",
+        name: "LUNCH BREAK",
+        round: ""
+    },
+    {
+        sl: 9,
+        timing: "02:30 A.M. - 5:30 P.M.",
+        name: "Tech Hunt",
+        round: "Final"
+    },
+    {
+        sl: 10,
+        timing: "2:30 P.M. - 5:30 P.M.",
+        name: "Tech Writing (C block Student Activity Room)",
+        round: "Final"
+    },
+    {
+        sl: 11,
+        timing: "2:30 P.M. - 5:30 P.M.",
+        name: "Ro-Terrance (B-Block 204)",
+        round: "Final"
+    },
+    {
+        sl: 11,
+        timing: "2:30 P.M. - 5:30 P.M.",
+        name: "Ro-Navigator (B Block 205)",
+        round: "Final"
+    },
+    {
+        sl: 11,
+        timing: "2:30 P.M. - 5:30 P.M.",
+        name: "Ro-Soccer (B-Block 202)",
+        round: "Final"
+    },
+    {
+        sl: 11,
+        timing: "2:30 P.M. - 5:30 P.M.",
+        name: "Aqua Race (C Block Boy’s",
+        round: "Final"
+    },
+    {
+        sl: 11,
+        timing: "2:30 P.M. - 5:30 P.M.",
+        name: "Common Room)",
+        round: "Final"
+    },
+    {
+        sl: 11,
+        timing: "2:30 P.M. - 5:30 P.M.",
+        name: "Khet (B Block 404)",
+        round: "Final"
+    }
+]
+
+export const day3 = [
+    {
+        sl: 1,
+        timing: "10:00 A.M. -12:30 P.M.",
+        name: "School Connect Programme- Technomania Junior (C Block Auditorium th Floor)",
+        round: " Final"
+    },
+    {
+        sl: 2,
+        timing: "12:30 A.M. -01:30 P.M",
+        name: "Workshop on Robotics(C Block Auditorium 7th Floor)",
+        round: ""
+    },
+    {
+        sl: 3,
+        timing: "1:30 P.M. - 2:30 P.M.",
+        name: "LUNCH BREAK",
+        round: ""
+    },
+    {
+        sl: 4,
+        timing: "2:30 P.M. - 3:00 P.M.",
+        name: "Prize Distribution Ceremony ( Phase I)",
+        round: ""
+    },
+    {
+        sl: 5,
+        timing: "3:00 P.M. - 4:00 P.M.",
+        name: "Prize Distribution Ceremony (Phase II)",
+        round: ""
+    },
+    {
+        sl: 6,
+        timing: "4:00 P.M. - 4:10 P.M",
+        name: "Vote of Thanks",
+        round: ""
+    },
+    {
+        sl: 7,
+        timing: "4:10 P.M. - 4:30 P.M",
+        name: "Cultural Programme",
+        round: ""
+    }
+]
